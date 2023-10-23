bc9e7243aa987b45ed651abdc0e5b0bf0c0933af
ayaka0530 <kanikamanosuke@outlook.com>
2023-10-23 11:55:40 +0900

    修正

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int Recursive(int n1, int n2,int n3, int hour) {
-	if (n1 < n3) {
+int SalaryCalc(int n1, int n2,int n3, int n4, int hour) {
+	if (n1 < n4) {
 
-		printf("%d時間で再帰的のほうが時給を超えた", hour);
+		printf("%d時間で再帰的のほうが給料を超えた", hour);
 		return (1);
 	}
 	else {
@@ -14,13 +14,14 @@ int Recursive(int n1, int n2,int n3, int hour) {
 			n1 = (1072 * hour);
 			n3 = n2;
 			n2 = (n2 * 2 - 50);
+			n4 = n4 + n3;
 		} 
 
 		printf("[%d時間目の時給]\n", hour);
-		printf("普通の時給%d\n", n1);
-		printf("再帰的な時給%d\n", n3);
-
-		return Recursive(n1, n2, n3, hour);
+		printf("普通の時給の時の給料 = %d\n", n1);
+		printf("再帰的な時給の時の給料 = %d\n", n4);
+		
+		return SalaryCalc(n1, n2, n3, n4, hour);
 	}
 
 }
@@ -30,9 +31,10 @@ int main() {
 	int num = 1072;
 	int num2 = 100;
 	int num3 = 0;
+	int num4 = 0;
 	int time = 0;
 
-	Recursive(num, num2,num3, time);
+	SalaryCalc(num, num2, num3, num4, time);
 
 
 	return 0;
