0149c695760fb9492c52b71ff8140f2fb6cbb801
hirokimaster <k022g0095@g.neec.ac.jp>
2023-10-30 12:16:55 +0900

    utf-8

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@
 
 typedef void (*pFunc)(int*, int*, int*);
 
+int Random(int time, int num) {
+	srand(time);
+    num = rand();
+	num = num % 6 + 1;
+	return num;
+}
+
 // コールバック関数
 void isCorrect(int* s, int* num, int* input) {
 	if (*input == 0 && *num % 2 == 1) {
@@ -29,18 +36,17 @@ void setTimeout(pFunc p, int second,int num, int input) {
 
 int main() {
 
-	int Time = time(nullptr);
-	srand(Time);
-	int num = rand();
-	num = num % 6 + 1;
-	int s; // 入力用
+	int Time = time(nullptr); // 時間
+	int saikoro = 0; // サイコロ
+	saikoro = Random(Time, saikoro);
+	int input; // 入力用
 
 	printf("奇数なら0偶数なら1を入力しよう\n");
-	scanf_s("%d", &s);
+	scanf_s("%d", &input);
 
 	pFunc p;
 	p = isCorrect;
-	setTimeout(p, 3,num, s);
+	setTimeout(p, 3,saikoro, input);
 
 	return 0;
 }
