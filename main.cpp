b6cdadc45a62476dfea4bc2da894282f456c8e85
aaaaa ttttt <ryudai.0728@gmail.com>
2023-11-06 03:55:23 +0900

    手直し

--- main.cpp
+++ main.cpp
@@ -1,41 +1,44 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <random>
 #include <functional>
 #include <Windows.h>
 
 int main() {
 
-	int Time = time(nullptr);
-	srand(Time);
-	int num = rand();
-	num = num % 6 + 1;
-	int s; 
+	// 乱数生成
+	std::random_device seed;
+	std::mt19937 randomEngine(seed());
+	std::uniform_int_distribution<int> dice(1, 6); // サイコロ
+	int input; // 入力用
+	int diceResult;	 // サイコロの結果
+	diceResult = dice(randomEngine);
 
 	// あってるかどうか判断する
-	std::function<int(int)> isCorrect = [=](int s) {
+	std::function<int(int)> isCorrect = [=](int input) {
 
+		
 		Sleep(3000);
 
-		if (s == 0 && num % 2 == 1) {
-			printf("さいころの目は%dなので\n正解!!", num);
+		if (input == 0 && diceResult % 2 == 1) {
+			printf("さいころの目は%dなので\n正解!!", diceResult);
 		}
-		else if (s == 1 && num % 2 == 0) {
-		    printf("さいころの目は%dなので\n正解!!", num);
+		else if (input == 1 && diceResult % 2 == 0) {
+		    printf("さいころの目は%dなので\n正解!!", diceResult);
 		}
 		else {
-			printf("さいころの目は%dなので\n不正解", num);
+			printf("さいころの目は%dなので\n不正解", diceResult);
 		}
 
-		return s;
+		return input;
 
 	};
 
 	
 
 	printf("奇数なら0偶数なら1を入力しよう\n");
-	scanf_s("%d", &s);
-	isCorrect(s);
+	scanf_s("%d", &input);
+	isCorrect(input);
 
 
 	return 0;
