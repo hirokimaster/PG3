5c53a907a1ebb0acc14deecfb7211bda626b8efa
aaaaa ttttt <ryudai.0728@gmail.com>
2023-11-06 03:44:30 +0900

    変数名変更

--- main.cpp
+++ main.cpp
@@ -1,19 +1,12 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <random>
 #include <Windows.h>
 
-typedef void (*pFunc)(int*, int*, int*);
+typedef void (*pFunc)(int*, int*);
 
-int Random(int time, int num) {
-	srand(time);
-    num = rand();
-	num = num % 6 + 1;
-	return num;
-}
-
-// コールバック関数
-void isCorrect(int* s, int* num, int* input) {
+// 合っているか
+void isCorrect(int* num, int* input) {
 	if (*input == 0 && *num % 2 == 1) {
 		printf("さいころの目は%dなので\n正解!!", *num);
 
@@ -28,17 +21,20 @@ void isCorrect(int* s, int* num, int* input) {
 	
 }
 
-void setTimeout(pFunc p, int second,int num, int input) {
-	Sleep(second * 1000);
-	p(&second, &num, &input);
+// 三秒後に表示する
+void setTimeout(pFunc p,int num, int input) {
+	Sleep(3000);
+	p( &num, &input);
 	
 }
 
 int main() {
 
-	int Time = time(nullptr); // 時間
-	int saikoro = 0; // サイコロ
-	saikoro = Random(Time, saikoro);
+	// サイコロ
+	std::random_device seed;
+	std::mt19937 randomEngine(seed());
+	std::uniform_int_distribution<int> saikoro(1, 6);
+
 	int input; // 入力用
 
 	printf("奇数なら0偶数なら1を入力しよう\n");
@@ -46,7 +42,7 @@ int main() {
 
 	pFunc p;
 	p = isCorrect;
-	setTimeout(p, 3,saikoro, input);
+	setTimeout(p, saikoro(randomEngine), input);
 
 	return 0;
 }
