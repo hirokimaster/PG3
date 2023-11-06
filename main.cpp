e4a38794916b87482a1d85b324977a547e873029
aaaaa ttttt <ryudai.0728@gmail.com>
2023-11-06 13:51:46 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,36 +1,37 @@
 ﻿#include <stdio.h>
 
+int salaryCalc(int salaryWage, int hour) {
+	return salaryWage * hour;
+}
+
+int recursiveSalaryCalc(int recursiveWage) {
+	return recursiveWage * 2 - 50;
+}
+
 
 // 給料をを計算して比較する
 int CompareSalaries(int salary, int recursiveWage, int recursiveSalary, int hour) {
 
 	// 再帰的な時給の給料が高くなったら終了
 	if (salary < recursiveSalary) {
-
-		printf("%d時間で再帰的のほうが給料を超えた", hour);
-		return (1);
+		return hour;
 	}
 
+	// 時間をカウント
 	hour++;
 
 	// 給料の計算
 	if (hour >= 1) {
-		salary = (1072 * hour);
+		salary = salaryCalc(1070, hour);
 
 		if (hour >= 2) {
-			recursiveWage = (recursiveWage * 2 - 50);
+			recursiveWage = recursiveSalaryCalc(recursiveWage);
 		}
 
 		recursiveSalary = recursiveSalary + recursiveWage;
 	}
-	
-	// 結果を表示
-	printf("[%d時間目の時給]\n", hour);
-	printf("普通の時給の時の給料 %d\n", salary);
-	printf("再帰的な時給の時の給料 %d\n", recursiveSalary);
-		
-	CompareSalaries(salary, recursiveWage, recursiveSalary, hour);
 
+	CompareSalaries(salary, recursiveWage, recursiveSalary, hour);
 }
 
 int main() {
@@ -39,9 +40,10 @@ int main() {
 	int recursiveWage = 100;
 	int recursiveSalary = 0;
 	int hour = 0;
-
-	CompareSalaries(salary, recursiveWage, recursiveSalary, hour);
-
+	
+	// 結果を表示
+	hour = CompareSalaries(salary, recursiveWage, recursiveSalary, hour);
+	printf("%d時間で再帰的な時給の給料が超えた", hour);
 
 	return 0;
 }
