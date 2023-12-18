fe610e56b472a21dd6fadbd2af332c4c54981a98
ニヘイリュウダイ <115063428+hirokimaster@users.noreply.github.com>
2023-12-18 14:32:43 +0900

    save

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main() {
 	std::vector<std::string> readFile;
 	std::string str;
 	while (std::getline(file, str)) {
-		std::cout << readFile.size() + 1 << std::endl;
+		std::cout << readFile.size() << std::endl;
 	}
 
 	return 0;
