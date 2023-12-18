30347bacf6ef1b64a95a9a6888d817ac058170e2
ニヘイリュウダイ <115063428+hirokimaster@users.noreply.github.com>
2023-12-18 17:16:42 +0900

    save

--- main.cpp
+++ main.cpp
@@ -3,16 +3,28 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <algorithm>
 
 int main() {
 
-	std::ifstream file;
-	file.open("PG3_05_02.txt", std::ios::in);
-	std::vector<std::string> readFile;
+	std::ifstream txtFile;
+	txtFile.open("PG3_05_02.ascii");
+	std::vector<std::string> readFileData;
+	std::vector<std::string>::iterator itr;
 	std::string str;
-	while (std::getline(file, str)) {
-		std::cout << readFile.size() << std::endl;
+	
+	while (std::getline(txtFile, str , ',')) {
+		readFileData.push_back(str);
 	}
+
+	txtFile.close();
+
+
+	
+	for (itr = readFileData.begin(); itr != readFileData.end(); ++itr) {
+		std::cout << *itr << std::endl;
+	}
+
 
 	return 0;
 	
